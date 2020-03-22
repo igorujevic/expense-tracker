1b1c275a6bc8effc4656329c08d5b2aa6c526893
igorujevic <iujevi00@fesb.hr>
2020-03-22 17:15:19 +0100

    all components and global state

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// components
+import { Header } from './components/Header.js';
+import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
+import { AddTransaction } from './components/AddTransaction.js';
+
+import { GlobalProvider } from './context/GlobalState';
+// css
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
